6e441c008d587d08151635dafdee3d8a870ea892
fater <faterazer@outlook.com>
2019-11-07 23:04:09 +0800

    Add code for problem 67.

---
+++ 0067. Add Binary/add_binary.cpp
@@ -0,0 +1,35 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+using std::reverse;
+using std::string;
+using std::cout;
+using std::endl;
+
+class Solution {
+public:
+    string addBinary(string a, string b);
+};
+
+string Solution::addBinary(string a, string b)
+{
+    auto aiter = a.rbegin();
+    auto biter = b.rbegin();
+    int carry = 0;
+    string ans = "";
+    while (aiter != a.rend() || biter != b.rend() || carry == 1) {
+        int adigit = 0, bdigit = 0;
+        if (aiter != a.rend()) {
+            adigit = *aiter == '1';
+            aiter++;
+        }
+        if (biter != b.rend()) {
+            bdigit = *biter == '1';
+            biter++;
+        }
+        ans.push_back(adigit ^ bdigit ^ carry + '0');
+        carry = (adigit + bdigit + carry) / 2;
+    }
+    reverse(ans.begin(), ans.end());
+    return ans;
+}
---
+++ 0067. Add Binary/add_binary.py
@@ -0,0 +1,29 @@
+class Solution:
+    def addBinary_MK1(self, a: str, b: str) -> str:
+        apos, bpos = len(a) - 1, len(b) - 1
+        carry = 0
+        ans = ""
+        while apos >= 0 or bpos >= 0 or carry:
+            adigit, bdigit = 0, 0
+            if apos >= 0:
+                adigit = int(a[apos])
+            if bpos >= 0:
+                bdigit = int(b[bpos])
+            ans = str(adigit ^ bdigit ^ carry) + ans
+            carry = (adigit + bdigit + carry) // 2
+            apos -= 1
+            bpos -= 1
+        return ans
+
+    def addBinary_MK2(self, a: str, b: str) -> str:
+        if len(a) == 0:
+            return b
+        elif len(b) == 0:
+            return a
+        elif a[-1] == '1' and b[-1] == '1':
+            return self.addBinary_MK2(self.addBinary_MK2(a[:-1], b[:-1]), '1') + '0'
+        else:
+            return self.addBinary_MK2(a[:-1], b[:-1]) + str(int(a[-1]) + int(b[-1]))
+
+    def addBinary_MK3(self, a: str, b: str) -> str:
+        return bin(eval('0b' + a) + eval('0b' + b))[2:]
