7c87d80f7b184930af3c215faaf89c347dc18c28
fater <faterazer@outlook.com>
2019-12-19 08:54:49 +0800

    提交 121 题代码及题解

---
+++ 0121. Best Time to Buy and Sell Stock/best_time_to_buy_and_sell_stock.cpp
@@ -0,0 +1,44 @@
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+
+class Solution {
+public:
+    int maxProfit_MK1(vector<int>& prices); // Solution.md: Approach 2: One pass
+    int maxProfit_MK2(vector<int>& prices);
+};
+
+// Solution.md
+// Approach 2: One pass
+int Solution::maxProfit_MK1(vector<int> &prices)
+{
+    if (prices.empty())
+        return 0;
+    int max = 0, min = prices[0];
+    for (size_t i = 1; i < prices.size(); i++) {
+        if (prices[i] < min)
+            min = prices[i];
+        else
+            max = std::max(max, prices[i] - min);
+    }
+    return max;
+}
+
+// My solution. Time complexity: O(n). Space complexity: O(1).
+// Calculate the difference array of prices. Ex: For [7, 1, 5, 3, 6, 4], the difference array
+// of prices is [0, -6, 4, -2, 3, -2], and then the problem converts to find the maximum subarray.
+// You can read Solution.md of Problem 53. Maximum Subarray to understand details.
+int Solution::maxProfit_MK2(vector<int> &prices)
+{
+    int max = 0, curr = 0;
+    for (size_t i = 1; i < prices.size(); i++) {
+        int diff = prices[i] - prices[i - 1];
+        if (diff > 0)
+            curr = curr >= 0 ? curr + diff : diff;
+        else
+            curr += diff;
+        max = std::max(max, curr);
+    }
+    return max;
+}
---
+++ 0121. Best Time to Buy and Sell Stock/best_time_to_buy_and_sell_stock.py
@@ -0,0 +1,35 @@
+from typing import List
+
+
+class Solution:
+    def maxProfit_MK1(self, prices: List[int]) -> int:
+        '''
+        Solution.md
+        Approach 2: One pass
+        '''
+        if not prices:
+            return 0
+        max_profit, min_price = 0, prices[0]
+        for i in range(1, len(prices)):
+            if prices[i] < min_price:
+                min_price = prices[i]
+            else:
+                max_profit = max(max_profit, prices[i] - min_price)
+        return max_profit
+
+    def maxProfit_MK2(self, prices: List[int]) -> int:
+        '''
+        My solution. Time complexity: O(n). Space complexity: O(1).
+        Calculate the difference array of prices. Ex: For [7, 1, 5, 3, 6, 4], the difference array
+        of prices is [0, -6, 4, -2, 3, -2], and then the problem converts to find the maximum subarray.
+        You can read Solution.md of Problem 53. Maximum Subarray to understand details.
+        '''
+        max_profit, max_curr = 0, 0
+        for i in range(1, len(prices)):
+            diff = prices[i] - prices[i-1]
+            if diff > 0:
+                max_curr = max_curr + diff if max_curr >= 0 else diff
+            else:
+                max_curr += diff
+            max_profit = max(max_profit, max_curr)
+        return max_profit
