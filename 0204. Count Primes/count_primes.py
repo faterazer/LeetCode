09ca3c2a05f81e14b244ad95a1d18a362316dcba
fater <faterazer@outlook.com>
2020-01-27 14:49:11 +0800

    提交 204 题代码及题解

---
+++ 0204. Count Primes/count_primes.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        if n < 2:
+            return 0
+
+        is_prime = [True] * n
+        is_prime[0] = is_prime[1] = False
+        for i in range(2, int(n ** 0.5) + 1):
+            if is_prime[i]:
+                is_prime[i * i:n:i] = [False] * len(is_prime[i * i:n:i])
+        return is_prime.count(True)
---
+++ 0204. Count Primes/题目描述(Description).md
@@ -0,0 +1,25 @@
+# 204. 计数质数
+
+统计所有小于非负整数 *n* 的质数的数量。
+
+**示例:**
+
+```()
+输入: 10
+输出: 4
+解释: 小于 10 的质数一共有 4 个, 它们是 2, 3, 5, 7 。
+```
+
+***
+
+# 204. Count Primes
+
+Count the number of prime numbers less than a non-negative number, ***n.***
+
+**Example:**
+
+```()
+Input: 10
+Output: 4
+Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
+```
