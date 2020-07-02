f40c2b6cdc4db9d902237572fa7a473f3595f2d1
fater <faterazer@outlook.com>
2020-07-02 12:09:30 +0800

    提交 459 题

---
+++ 0459. Repeated Substring Pattern/Solution.java
@@ -0,0 +1,6 @@
+public class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        String str = s + s;
+        return str.substring(1, str.length() - 1).contains(s);
+    }
+}
