2627abe953bf41c42549160377f6558628bc009b
fater <faterazer@outlook.com>
2020-08-03 09:37:56 +0800

    提交 415 题

---
+++ 0415. Add Strings/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String addStrings(String num1, String num2) {
+        StringBuilder res = new StringBuilder();
+        int carry = 0;
+        for (int i = num1.length() - 1, j = num2.length() - 1; i >= 0 || j >= 0 || carry != 0; i--, j--) {
+            int a = i >= 0 ? num1.charAt(i) - '0' : 0;
+            int b = j >= 0 ? num2.charAt(j) - '0' : 0;
+            carry += a + b;
+            res.append(carry % 10);
+            carry /= 10;
+        }
+        res.reverse();
+        return res.toString();
+    }
+}
