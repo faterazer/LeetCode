0e0abc7881f1c0dd80b30e77770b2df5a92f9902
fater <faterazer@outlook.com>
2020-08-25 15:35:50 +0800

    提交 404 题

---
+++ 0404. Sum of Left Leaves/Solution.java
@@ -0,0 +1,25 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (root == null)
+            return 0;
+        if (root.left != null && root.left.left == null && root.left.right == null)
+            return root.left.val + this.sumOfLeftLeaves(root.right);
+        return this.sumOfLeftLeaves(root.left) + this.sumOfLeftLeaves(root.right);
+    }
+}
