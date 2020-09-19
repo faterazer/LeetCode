62056286d97d2ad2daa85cd2401b0a2bab17602f
faterazer <faterazer@outlook.com>
2020-09-19 15:52:19 +0800

    提交 404 题

---
+++ 0404. Sum of Left Leaves/Solution.c
@@ -0,0 +1,14 @@
+struct TreeNode
+{
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+int sumOfLeftLeaves(struct TreeNode *root) {
+    if (!root)
+        return 0;
+    if (root->left && !root->left->left && !root->left->right)
+        return root->left->val + sumOfLeftLeaves(root->right);
+    return sumOfLeftLeaves(root->left) + sumOfLeftLeaves(root->right);
+}
