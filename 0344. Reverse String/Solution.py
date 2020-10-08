6166bbc26faa0e6bedd3c4d01f9ee8edf8c7c220
faterazer <faterazer@outlook.com>
2020-10-08 10:27:34 +0800

    提交 344 题

---
+++ 0344. Reverse String/Solution.py
@@ -0,0 +1,10 @@
+class Solution {
+    public void reverseString(char[] s) {
+
+    }
+}from typing import List
+
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        s.reverse()
--- 0344. Reverse String/reverse_string.py
+++
@@ -1,6 +0,0 @@
-from typing import List
-
-
-class Solution:
-    def reverseString(self, s: List[str]) -> None:
-        s.reverse()
