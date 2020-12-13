5c6a807a832120eb361863651250536088250a72
faterazer <faterazer@outlook.com>
2020-12-13 13:11:56 +0800

    提交 217 题

---
+++ 0217. Contains Duplicate/Solution.c
@@ -0,0 +1,16 @@
+#include <stdbool.h>
+#include <stdlib.h>
+
+int cmp(const void *a, const void *b)
+{
+    return *(int*)a - *(int*)b;
+}
+
+bool containsDuplicate(int *nums, int numsSize)
+{
+    qsort(nums, numsSize, sizeof(int), cmp);
+    for (int i = 1; i < numsSize; i++)
+        if (nums[i] == nums[i - 1])
+            return true;
+    return false;
+}
---
+++ 0217. Contains Duplicate/Solution.java
@@ -0,0 +1,12 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+        return set.size() != nums.length;
+    }
+}
