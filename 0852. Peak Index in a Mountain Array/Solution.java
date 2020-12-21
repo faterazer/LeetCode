913f6947496b51e37daf3dd448422204152952fa
faterazer <faterazer@outlook.com>
2020-12-21 11:50:13 +0800

    提交 852 题

---
+++ 0852. Peak Index in a Mountain Array/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int left = 0, right = arr.length - 1, mid;
+        while (left < right) {
+            mid = left + ((right - left) >> 1);
+            if (arr[mid] < arr[mid + 1])
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        return left;
+    }
+}
