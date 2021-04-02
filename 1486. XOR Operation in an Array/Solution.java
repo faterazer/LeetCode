cad342e4b7843bd396872f2d6c16a088d127f3f6
faterazer <faterazer@outlook.com>
2021-04-02 13:38:12 +0800

    提交 1486 题

--- 1486. XOR Operation in an Array/Solution.java
+++ 1486. XOR Operation in an Array/Solution.java
@@ -1,9 +1,28 @@
 class Solution {
-    public int xorOperation(int n, int start) {
+    public int xorOperation_MK1(int n, int start) {
         int res = 0;
         for (int i = 0; i < n; i++, start += 2) {
             res ^= start;
         }
         return res;
+    }
+
+    public int xorOperation_MK2(int n, int start) {
+        int res = helper(n, start >> 1) << 1;
+        if ((n & start & 1) == 1) {
+            res |= 1;
+        }
+        return res;
+    }
+
+    private int helper(int n, int start) {
+        if (start % 2 == 1) {
+            return (start - 1) ^ helper(n + 1, start - 1);
+        }
+        if (n % 2 == 0) {
+            return (n / 2) & 1;
+        } else {
+            return (n / 2 ) & 1 ^ (start + n - 1);
+        }
     }
 }
