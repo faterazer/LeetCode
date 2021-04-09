cb74b116d9b74e55b50200e68f8cc94fb579dbb2
fater <faterazer@outlook.com>
2021-04-09 21:29:14 +0800

    提交 1486 题

--- 1486. XOR Operation in an Array/Solution.py
+++ 1486. XOR Operation in an Array/Solution.py
@@ -1,6 +1,20 @@
 class Solution:
-    def xorOperation(self, n: int, start: int) -> int:
+    def xorOperation_MK1(self, n: int, start: int) -> int:
         res = 0
         for x in range(start, start + n * 2, 2):
             res ^= x
+        return res
+
+    def xorOPeration_MK2(self, n: int, start: int) -> int:
+        def helper(n: int, start: int) -> int:
+            if start & 1:
+                return (start - 1) ^ helper(n + 1, start - 1)
+            if n % 2:
+                return n >> 1 & 1 ^ (start + n - 1)
+            else:
+                return n >> 1 & 1
+
+        res = helper(n, start >> 1) << 1
+        if n & start & 1:
+            res |= 1
         return res
