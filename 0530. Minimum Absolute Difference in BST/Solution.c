b3856ea3da0796b812876bb2c6a7fb1aa0983350
fater <faterazer@outlook.com>
2021-04-13 22:08:33 +0800

    提交 530 题

--- 0530. Minimum Absolute Difference in BST/Solution.c
+++ 0530. Minimum Absolute Difference in BST/Solution.c
@@ -10,6 +10,46 @@ struct TreeNode
     struct TreeNode *right;
 };
 
+void inOrder(struct TreeNode *root, struct TreeNode **pre, int *ans);
+struct TreeNode* getSuccessor(struct TreeNode *root);
+
+// Recurive
+int getMinimumDifference_MK1(struct TreeNode *root)
+{
+    struct TreeNode *pre = NULL;
+    int ans = INT_MAX;
+    inOrder(root, &pre, &ans);
+    return ans;
+}
+
+// Mirrors
+int getMinimumDifference_MK2(struct TreeNode *root)
+{
+    int ans = INT_MAX;
+    struct TreeNode *pre = NULL;
+    while (root) {
+        if (pre)
+            ans = min(ans, abs(root->val - pre->val));
+        if (!root->left) {
+            pre = root;
+            root = root->right;
+        }
+        else {
+            struct TreeNode *succ = getSuccessor(root);
+            if (!succ->right) {
+                succ->right = root;
+                root = root->left;
+            }
+            else {
+                succ->right = NULL;
+                pre = root;
+                root = root->right;
+            }
+        }
+    }
+    return ans;
+}
+
 void inOrder(struct TreeNode *root, struct TreeNode **pre, int *ans)
 {
     if (root->left)
@@ -21,10 +61,10 @@ void inOrder(struct TreeNode *root, struct TreeNode **pre, int *ans)
         inOrder(root->right, pre, ans);
 }
 
-int getMinimumDifference(struct TreeNode *root)
+struct TreeNode* getSuccessor(struct TreeNode *root)
 {
-    struct TreeNode *pre = NULL;
-    int ans = INT_MAX;
-    inOrder(root, &pre, &ans);
-    return ans;
+    struct TreeNode *p = root->left;
+    while (p->right && p->right != root)
+        p = p->right;
+    return p;
 }
--- 0530. Minimum Absolute Difference in BST/Solution.cpp
+++ 0530. Minimum Absolute Difference in BST/Solution.cpp
@@ -13,11 +13,38 @@ struct TreeNode
 class Solution
 {
 public:
-    int getMinimumDifference(TreeNode *root)
+    int getMinimumDifference_MK1(TreeNode *root)
     {
         TreeNode *pre = nullptr;
         int ans = numeric_limits<int>::max();
         inorder(root, pre, ans);
+        return ans;
+    }
+
+    int getMinimumDifference_MK2(TreeNode *root)
+    {
+        int ans = numeric_limits<int>::max();
+        TreeNode *pre = nullptr;
+        while (root) {
+            if (pre)
+                ans = min(ans, abs(root->val - pre->val));
+            if (!root->left) {
+                pre = root;
+                root = root->right;
+            }
+            else {
+                TreeNode *succ = getSuccessor(root);
+                if (!succ->right) {
+                    succ->right = root;
+                    root = root->left;
+                }
+                else {
+                    succ->right = nullptr;
+                    pre = root;
+                    root = root->right;
+                }
+            }
+        }
         return ans;
     }
 
@@ -31,5 +58,13 @@ private:
             ans = min(abs(root->val - pre->val), ans);
         pre = root;
         inorder(root->right, pre, ans);
+    }
+
+    TreeNode* getSuccessor(TreeNode *root)
+    {
+        TreeNode *p = root->left;
+        while (p->right && p->right != root)
+            p = p->right;
+        return p;
     }
 };
