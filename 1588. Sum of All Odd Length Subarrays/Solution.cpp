527e936d2107eeb12ae722aba37387aeacb35d02
faterazer <faterazer@outlook.com>
2021-04-16 11:04:52 +0800

    提交 1588 题

---
+++ 1588. Sum of All Odd Length Subarrays/Solution.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    int sumOddLengthSubarrays(vector<int> &arr)
+    {
+        int res = 0, n = arr.size();
+        for (int i = 0; i < n; i++)
+            res += ((i + 1) * (n - i) + 1) / 2 * arr[i];
+        return res;
+    }
+};
