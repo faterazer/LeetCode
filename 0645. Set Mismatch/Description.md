a3305e249ad4538c042e92161bc5f3d5940b3192
yubowen.1213 <yubowen.1213@bytedance.com>
2021-07-04 11:19:34 +0800

    提交 645 题

--- 0645. Set Mismatch/Description.md
+++ 0645. Set Mismatch/Description.md
@@ -1,6 +1,6 @@
 # 645. Set Mismatch
 
-The set `S` originally contains numbers from 1 to `n`. But unfortunately, due to the data error, one of the numbers in the set got duplicated to **another** number in the set, which results in repetition of one number and loss of another number.
+The set `S` originally contains numbers from `1` to `n`. But unfortunately, due to the data error, one of the numbers in the set got duplicated to **another** number in the set, which results in repetition of one number and loss of another number.
 
 Given an array `nums` representing the data status of this set after the error. Your task is to firstly find the number occurs twice and then find the number that is missing. Return them in the form of an array.
 
--- 0645. Set Mismatch/Solution.c
+++ 0645. Set Mismatch/Solution.c
@@ -5,11 +5,10 @@
 
 // Time complexity: O(n)
 // Space complexity: O(1)
-int *findErrorNums_MK1(int *nums, int numsSize, int *returnSize)
+int* findErrorNums_MK1(int *nums, int numsSize, int *returnSize)
 {
     int sum = 0, dup = 0;
-    for (int i = 0; i < numsSize; i++)
-    {
+    for (int i = 0; i < numsSize; i++) {
         if (nums[abs(nums[i]) - 1] < 0)
             dup = abs(nums[i]);
         else
@@ -26,7 +25,7 @@ int *findErrorNums_MK1(int *nums, int numsSize, int *returnSize)
 // Time complexity: O(n)
 // Space complexity: O(1)
 // Note: (n * (n + 1) * (2 * n + 1) / 6 = 1^2 + 2^2 +...+ n^2
-int *findErrorNums_MK2(int *nums, int numsSize, int *returnSize)
+int* findErrorNums_MK2(int *nums, int numsSize, int *returnSize)
 {
     *returnSize = 2;
     long long sum = 0, sum2 = 0, n = numsSize;
@@ -40,4 +39,44 @@ int *findErrorNums_MK2(int *nums, int numsSize, int *returnSize)
     ret[0] = (b - a) >> 1;
     ret[1] = (b + a) >> 1;
     return ret;
+}
+
+// Time complexity: O(n)
+// Space complexity: O(1)
+// 位运算
+int* findErrorNums_MK3(int *nums, int numsSize, int *returnSize)
+{
+    int xor = 0;
+    for (int i = 0; i < numsSize; i++)
+        xor ^= nums[i];
+    for (int i = 1; i <= numsSize; i++)
+        xor ^= i;
+
+    int lowbit = xor & (-xor);
+    int num1 = 0, num2 = 0;
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] & lowbit)
+            num1 ^= nums[i];
+        else
+            num2 ^= nums[i];
+    }
+    for (int i = 1; i <= numsSize; i++) {
+        if (i & lowbit)
+            num1 ^= i;
+        else
+            num2 ^= i;
+    }
+
+    int *res = (int*)malloc(sizeof(int) * 2);
+    *returnSize = 2;
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] == num1) {
+            res[0] = num1;
+            res[1] = num2;
+            return res;
+        }
+    }
+    res[0] = num2;
+    res[1] = num1;
+    return res;
 }
