10870e68494066c8928fd1d176cc25d2f1c5f0ff
yubowen.1213 <yubowen.1213@bytedance.com>
2022-01-19 22:46:58 +0800

    提交 219 题

---
+++ 0219. Contains Duplicate II/Solution.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean containsNearbyDuplicate_MK1(int[] nums, int k) {
+        // Use map
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (!map.containsKey(nums[i])) {
+                map.put(nums[i], i);
+            } else if (Math.abs(i - map.get(nums[i])) <= k) {
+                return true;
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return false;
+    }
+
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (i > k) {
+                set.remove(nums[i - k - 1]);
+            }
+            if (set.contains(nums[i])) {
+                return true;
+            }
+            set.add(nums[i]);
+        }
+        return false;
+    }
+}
