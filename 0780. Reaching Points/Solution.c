f0455dd9a18b53acad124ed0a4c457afeba5774c
fater <faterazer@outlook.com>
2022-04-10 16:05:58 +0800

    780：增加 C 代码

---
+++ 0780. Reaching Points/Solution.c
@@ -0,0 +1,12 @@
+#include <stdbool.h>
+
+bool reachingPoints(int sx, int sy, int tx, int ty)
+{
+    while (sx < tx && sy < ty) {
+        if (tx < ty)
+            ty = ty % tx;
+        else
+            tx = tx % ty;
+    }
+    return (sx == tx && sy <= ty && (ty - sy) % sx == 0) || (sy == ty && sx <= tx && (tx - sx) % sy == 0);
+}
