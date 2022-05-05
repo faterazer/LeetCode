d2d44b2e00acd01563a34ed8dc25414f7a2af855
fater <faterazer@outlook.com>
2022-05-05 13:04:05 +0800

    225：增加 Java 代码

---
+++ 0225. Implement Stack using Queues/Solution.java
@@ -0,0 +1,39 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+class MyStack {
+    private final Queue<Integer> queue;
+
+    public MyStack() {
+        queue = new LinkedList<>();
+    }
+
+    public void push(int x) {
+        int n = queue.size();
+        queue.offer(x);
+        for (int i = 0; i < n; i++) {
+            queue.offer(queue.poll());
+        }
+    }
+
+    public int pop() {
+        return queue.remove();
+    }
+
+    public int top() {
+        return queue.element();
+    }
+
+    public boolean empty() {
+        return queue.isEmpty();
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
