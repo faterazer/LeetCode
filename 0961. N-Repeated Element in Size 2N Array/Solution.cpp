7006bfdb6e23e8da861d890024adc11ae7e60017
fater <faterazer@outlook.com>
2022-05-21 22:52:34 +0800

    961: 增加随机方法

--- 0961. N-Repeated Element in Size 2N Array/Solution.cpp
+++ 0961. N-Repeated Element in Size 2N Array/Solution.cpp
@@ -1,13 +1,26 @@
+#include <random>
 #include <vector>
 using namespace std;
 
-class Solution
-{
+class Solution {
 public:
-    int repeatedNTimes(vector<int> &A)
+    int repeatedNTimes_MK1(vector<int>& A)
     {
         for (size_t i = 2; i < A.size(); i++)
             if (A[i] == A[i - 1] || A[i] == A[i - 2])
                 return A[i];
+        return A[0];
+    }
+
+    int repeatedNTimes_MK2(vector<int>& A)
+    {
+        default_random_engine e;
+        uniform_int_distribution<unsigned> dis(0, A.size() - 1);
+        for (int i = 0; i < 10; ++i) {
+            int x = dis(e), y = dis(e);
+            if (x != y && A[x] == A[y])
+                return A[x];
+        }
+        return repeatedNTimes_MK1(A);
     }
 };
