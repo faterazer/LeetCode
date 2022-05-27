9410e0b0e77f40eb806eb355b6ef2fcf061bce53
fater <faterazer@outlook.com>
2022-05-27 15:43:42 +0800

    961题：增加 Python 代码

--- 0961. N-Repeated Element in Size 2N Array/Solution.py
+++ 0961. N-Repeated Element in Size 2N Array/Solution.py
@@ -1,10 +1,18 @@
+import random
 from typing import List
 
 
 class Solution:
-    def repeatedNTimes(self, A: List[int]) -> int:
+    def repeatedNTimes_MK1(self, A: List[int]) -> int:
         for i in range(2, len(A)):
             if A[i] == A[i - 1] or A[i] == A[i - 2]:
                 return A[i]
         return A[0]
-        
+
+    def repeatedNTimes_MK2(self, A: List[int]) -> int:
+        n = len(A)
+        for _ in range(10):
+            x, y = random.randrange(n), random.randrange(n)
+            if x != y and A[x] == A[y]:
+                return A[x]
+        return self.repeatedNTimes_MK1(A)
