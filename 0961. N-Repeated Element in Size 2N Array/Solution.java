4de796101514bae24acb0209500726bb57734968
fater <faterazer@outlook.com>
2022-06-02 16:45:43 +0800

    961题：增加 Java 代码

--- 0961. N-Repeated Element in Size 2N Array/Solution.java
+++ 0961. N-Repeated Element in Size 2N Array/Solution.java
@@ -1,9 +1,24 @@
+import java.util.Random;
+
 class Solution {
-    public int repeatedNTimes(int[] A) {
+    public int repeatedNTimes_MK1(int[] A) {
         for (int i = 2; i < A.length; i++) {
             if (A[i] == A[i - 1] || A[i] == A[i - 2])
                 return A[i];
         }
         return A[0];
+    }
+
+    public int repeatedNTimes_MK2(int[] A) {
+        int n = A.length;
+        Random random = new Random();
+
+        for (int i = 0; i < 10; ++i) {
+            int x = random.nextInt(n), y = random.nextInt(n);
+            if (x != y && A[x] == A[y]) {
+                return A[x];
+            }
+        }
+        return repeatedNTimes_MK1(A);
     }
 }
