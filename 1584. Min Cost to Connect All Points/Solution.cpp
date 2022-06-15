4ce3f2b3e71ba380c2795402229aaa237acddee5
yubowen.ssr <yubowen.ssr@bytedance.com>
2022-06-15 11:20:47 +0800

    1584: 增加 CPP 代码

--- 1584. Min Cost to Connect All Points/Solution.cpp
+++ 1584. Min Cost to Connect All Points/Solution.cpp
@@ -1,4 +1,9 @@
 #include <algorithm>
+#include <functional>
+#include <queue>
+#include <random>
+#include <set>
+#include <string>
 #include <vector>
 using namespace std;
 
@@ -12,9 +17,9 @@ struct Edge {
     {
     }
 
-    bool operator<(const Edge& a) const
+    bool operator<(const Edge& right) const
     {
-        return len < a.len;
+        return len < right.len;
     }
 };
 
@@ -30,16 +35,15 @@ struct Pos {
 
     bool operator<(const Pos& right) const
     {
-        return x == right.x ? y < right.y : x < right.x;
+        return x != right.x ? x < right.x : y < right.y;
     }
 };
 
 class BIT {
 public:
-    BIT(int _n)
-        : n(_n)
-        , tree(_n, INT_MAX)
-        , id_rec(_n, -1)
+    BIT(int n)
+        : tree(n, INT_MAX)
+        , id_rec(n, -1)
     {
     }
 
@@ -48,50 +52,47 @@ public:
         return x & -x;
     }
 
-    void update(int pos, int val, int id)
+    int query(int idx)
     {
-        while (pos > 0) {
-            if (tree[pos] > val) {
-                tree[pos] = min(tree[pos], val);
-                id_rec[pos] = id;
+        int minval = INT_MAX, res = -1, n = tree.size();
+        while (idx < n) {
+            if (tree[idx] < minval) {
+                minval = tree[idx];
+                res = id_rec[idx];
             }
-            pos -= lowbit(pos);
-        }
-    }
-
-    int query(int pos)
-    {
-        int minval = INT_MAX, res = -1;
-        while (pos < n) {
-            if (tree[pos] < minval) {
-                minval = tree[pos];
-                res = id_rec[pos];
-            }
-            pos += lowbit(pos);
+            idx += lowbit(idx);
         }
         return res;
     }
 
+    void update(int idx, int val, int id)
+    {
+        while (idx > 0) {
+            if (tree[idx] > val) {
+                tree[idx] = val;
+                id_rec[idx] = id;
+            }
+            idx -= lowbit(idx);
+        }
+    }
+
 private:
-    int n;
     vector<int> tree, id_rec;
 };
 
 class DisjointSetUnion {
 public:
-    DisjointSetUnion(int _n)
-        : tree(_n, -1)
-        , rank(_n, 1)
+    DisjointSetUnion(int n)
+        : tree(n, -1)
     {
     }
 
-    bool unionSet(int x, int y)
+    bool unionSet(int a, int b)
     {
-        int root_x = findRoot(x), root_y = findRoot(y);
-        if (root_x == root_y)
+        int root_a = findRoot(a), root_b = findRoot(b);
+        if (root_a == root_b)
             return false;
-        tree[root_y] = root_x;
-        rank[root_x] += rank[root_y];
+        tree[root_b] = root_a;
         return true;
     }
 
@@ -105,7 +106,7 @@ private:
         return root;
     }
 
-    vector<int> tree, rank;
+    vector<int> tree;
 };
 
 class Solution {
@@ -115,20 +116,15 @@ public:
         vector<Edge> edges;
         for (size_t i = 0; i < points.size(); ++i)
             for (size_t j = i + 1; j < points.size(); ++j)
-                edges.emplace_back(Edge(i, j, manhattanDistance(points[i], points[j])));
-        sort(edges.begin(), edges.end(), [](const Edge& a, const Edge& b) {
-            return a.len < b.len;
-        });
-        vector<int> tree(points.size(), -1);
-        int count = tree.size(), res = 0;
-        for (size_t i = 0; i < edges.size() && count > 1; ++i) {
-            int root_a = findRoot(tree, edges[i].x);
-            int root_b = findRoot(tree, edges[i].y);
-            int dist = edges[i].len;
-            if (root_a != root_b) {
-                res += dist;
-                count -= 1;
-                tree[root_b] = root_a;
+                edges.emplace_back(i, j, abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1]));
+        sort(edges.begin(), edges.end());
+
+        int res = 0, cnt = 1, n = points.size();
+        DisjointSetUnion dsu(n);
+        for (size_t i = 0; i < edges.size() && cnt < n; ++i) {
+            if (dsu.unionSet(edges[i].x, edges[i].y)) {
+                res += edges[i].len;
+                ++cnt;
             }
         }
         return res;
@@ -136,56 +132,53 @@ public:
 
     int minCostConnectPoints_MK2(vector<vector<int>>& points) // Prim
     {
-        int res = 0, count = 0;
-        vector<int> in_MST(points.size(), 0);
-        vector<int> min_dist(points.size(), INT_MAX);
+        int n = points.size(), cnt = 0, res = 0;
+        vector<bool> in_MST(n, false);
+        vector<int> min_dist(n, INT_MAX);
         min_dist[0] = 0;
-        while (count < points.size()) {
+        while (cnt < n) {
             int cur_node = -1, cur_dist = INT_MAX;
-            for (int i = 0; i < min_dist.size(); ++i) {
+            for (int i = 0; i < n; ++i) {
                 if (!in_MST[i] && min_dist[i] < cur_dist) {
                     cur_node = i;
                     cur_dist = min_dist[i];
                 }
             }
-            in_MST[cur_node] = 1;
+            in_MST[cur_node] = true;
+            min_dist[cur_node] = cur_dist;
             res += cur_dist;
-            count += 1;
-            for (int i = 0; i < points.size(); ++i) {
-                if (in_MST[i])
-                    continue;
-                int dist = manhattanDistance(points[cur_node], points[i]);
-                if (dist < min_dist[i])
-                    min_dist[i] = dist;
-            }
+            ++cnt;
+            for (int i = 0; i < n; ++i)
+                if (!in_MST[i])
+                    min_dist[i] = min(min_dist[i], abs(points[i][0] - points[cur_node][0]) + abs(points[i][1] - points[cur_node][1]));
         }
         return res;
     }
 
     int minCostConnectPoints_MK3(vector<vector<int>>& points) // Kruskal (Optimized)
     {
-        vector<Edge> edges = build_edges(points);
-
-        int n = points.size();
-        DisjointSetUnion dsu(n);
+        vector<Edge> edges = buildEdges(points);
         sort(edges.begin(), edges.end());
-        int res = 0, count = n;
-        for (size_t i = 0; i < edges.size() && count; ++i) {
+
+        int res = 0, cnt = 1, n = points.size();
+        DisjointSetUnion dsu(n);
+        for (size_t i = 0; i < edges.size() && cnt < n; ++i) {
             if (dsu.unionSet(edges[i].x, edges[i].y)) {
                 res += edges[i].len;
-                --count;
+                ++cnt;
             }
         }
         return res;
     }
 
 private:
-    vector<Edge> build_edges(const vector<vector<int>>& points)
+    vector<Edge> buildEdges(vector<vector<int>>& points)
     {
         vector<Pos> pos;
-        for (size_t i = 0; i < points.size(); ++i)
-            pos.emplace_back(points[i][0], points[i][1], i);
         vector<Edge> edges;
+        pos.reserve(points.size());
+        for (size_t i = 0; i < points.size(); ++i)
+            pos.emplace_back(Pos(points[i][0], points[i][1], i));
         build(pos, edges);
         for (Pos& p : pos)
             swap(p.x, p.y);
@@ -202,35 +195,20 @@ private:
     void build(vector<Pos>& pos, vector<Edge>& edges)
     {
         sort(pos.begin(), pos.end());
-        int n = pos.size();
-        vector<int> idx_map(n);
-        for (int i = 0; i < n; ++i)
-            idx_map[i] = pos[i].y - pos[i].x;
-        sort(idx_map.begin(), idx_map.end());
-        idx_map.erase(unique(idx_map.begin(), idx_map.end()), idx_map.end());
-        BIT bit(idx_map.size() + 1);
-        for (int i = n - 1; i >= 0; --i) {
-            int idx = lower_bound(idx_map.begin(), idx_map.end(), pos[i].y - pos[i].x) - idx_map.begin() + 1;
-            int point_id = bit.query(idx);
-            if (point_id != -1) {
-                int distance = abs(pos[point_id].x - pos[i].x) + abs(pos[point_id].y - pos[i].y);
-                edges.emplace_back(pos[i].id, pos[point_id].id, distance);
-            }
+        vector<int> id_map;
+        id_map.reserve(pos.size());
+        for (const Pos& p : pos)
+            id_map.emplace_back(p.y - p.x);
+        sort(id_map.begin(), id_map.end());
+        id_map.erase(unique(id_map.begin(), id_map.end()), id_map.end());
+
+        BIT bit(id_map.size() + 1);
+        for (int i = pos.size() - 1; i >= 0; --i) {
+            int idx = lower_bound(id_map.begin(), id_map.end(), pos[i].y - pos[i].x) - id_map.begin() + 1;
+            int j = bit.query(idx);
+            if (j != -1)
+                edges.emplace_back(Edge(pos[i].id, pos[j].id, abs(pos[i].x - pos[j].x) + abs(pos[i].y - pos[j].y)));
             bit.update(idx, pos[i].x + pos[i].y, i);
         }
-    }
-
-    int manhattanDistance(const vector<int>& a, const vector<int>& b)
-    {
-        return abs(a[0] - b[0]) + abs(a[1] - b[1]);
-    }
-
-    int findRoot(vector<int>& tree, int x)
-    {
-        if (tree[x] == -1)
-            return x;
-        int res = findRoot(tree, tree[x]);
-        tree[x] = res;
-        return res;
     }
 };
