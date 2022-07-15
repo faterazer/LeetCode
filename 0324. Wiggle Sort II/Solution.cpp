8fd85d227a3a467b711aadc5f64f5cac5b0f7722
yubowen.ssr <yubowen.ssr@bytedance.com>
2022-07-15 12:02:53 +0800

    324: 修改 cpp 代码

--- 0324. Wiggle Sort II/Solution.cpp
+++ 0324. Wiggle Sort II/Solution.cpp
@@ -2,21 +2,22 @@
 #include <vector>
 using namespace std;
 
-#define A(i) nums[(1 + 2 * (i)) % (n | 1)]
-
 class Solution {
 public:
     void wiggleSort(vector<int>& nums)
     {
         int n = nums.size();
-        auto mid_ptr = nums.begin() + n / 2;
-        nth_element(nums.begin(), mid_ptr, nums.end());
-        int mid = *mid_ptr;
+        int mid_idx = n / 2;
+        nth_element(nums.begin(), nums.begin() + mid_idx, nums.end());
+        int mid = nums[mid_idx];
 
+        auto A = [&](int x) -> int& {
+            return nums[(2 * x + 1) % (n | 1)];
+        };
         int i = 0, j = 0, k = n - 1;
         while (j <= k) {
             if (A(j) > mid)
-                swap(A(j++), A(i++));
+                swap(A(i++), A(j++));
             else if (A(j) < mid)
                 swap(A(j), A(k--));
             else
