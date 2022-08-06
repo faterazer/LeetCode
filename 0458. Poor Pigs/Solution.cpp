c76b2cbfd557e4e8ca5c84ba23fb548ecbb1f47c
fater <faterazer@outlook.com>
2022-08-06 20:04:12 +0800

    提交 458 题

---
+++ 0458. Poor Pigs/Solution.cpp
@@ -0,0 +1,9 @@
+#include <cmath>
+
+class Solution {
+public:
+    int poorPigs(int buckets, int minutesToDie, int minutesToTest)
+    {
+        return ceil(log(buckets) / log(minutesToTest / minutesToDie + 1));
+    }
+};
