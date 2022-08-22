ac7d04fb0ad535e675eecf1187f61c9dbe3a49f8
yubowen.ssr <yubowen.ssr@bytedance.com>
2022-08-22 11:56:13 +0800

    458: refine code

--- 0458. Poor Pigs/Solution.java
+++ 0458. Poor Pigs/Solution.java
@@ -1,5 +1,5 @@
 class Solution {
     public int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
-        return (int) Math.ceil(Math.log(buckets) / Math.log((double) minutesToTest / minutesToDie + 1));
+        return (int) Math.ceil(Math.log(buckets) / Math.log((double) minutesToTest / minutesToDie + 1) - 1e-5);
     }
 }
