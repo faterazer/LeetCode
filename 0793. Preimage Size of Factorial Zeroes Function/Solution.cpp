4f42cef9742e443b9d45e1ef3a315a12015309eb
fater <faterazer@outlook.com>
2022-09-01 22:29:53 +0800

    提交 793 题

---
+++ 0793. Preimage Size of Factorial Zeroes Function/Solution.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int preimageSizeFZF(int k)
+    {
+        long long l = 0, r = 5ll * k;
+        while (l <= r) {
+            long long m = l + ((r - l) >> 1);
+            int zeros = numOfTrailingZeros(m);
+
+            if (zeros == k)
+                return 5;
+            else if (zeros < k)
+                l = m + 1;
+            else
+                r = m - 1;
+        }
+        return 0;
+    }
+
+private:
+    long long numOfTrailingZeros(long long x)
+    {
+        long long res = 0;
+        for (; x; x /= 5)
+            res += x / 5;
+        return res;
+    }
+};
