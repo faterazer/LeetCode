4aa004ea8589f7ab456124e748643e89e5508017
yubowen.ssr <yubowen.ssr@bytedance.com>
2022-10-14 12:06:43 +0800

    769：增加 Java 代码

---
+++ 0769. Max Chunks To Make Sorted/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxChunksToSorted(int[] arr) {
+        int res = 0, max = 0;
+        for (int i = 0; i < arr.length; i++) {
+            max = Math.max(max, arr[i]);
+            if (max == i) {
+                ++res;
+            }
+        }
+        return res;
+    }
+}
