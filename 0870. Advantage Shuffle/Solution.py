7034b2688b593d4e25f11dbf426a47e369c9d92f
fater <faterazer@outlook.com>
2022-10-24 10:46:21 +0800

    870：增加 Python 代码

--- 0870. Advantage Shuffle/Solution.py
+++ 0870. Advantage Shuffle/Solution.py
@@ -3,15 +3,16 @@ from typing import List
 
 class Solution:
     def advantageCount(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        nums1.sort(reverse=True)
-        buff = sorted([(e, i) for i, e in enumerate(nums2)], key=lambda x: x[0], reverse=True)
-        lo, hi = 0, len(nums1) - 1
-        res = [0] * len(nums1)
-        for e, idx in buff:
-            if nums1[lo] > e:
-                res[idx] = nums1[lo]
-                lo += 1
-            else:
-                res[idx] = nums1[hi]
+        n = len(nums1)
+        nums1.sort()
+        buff = sorted(zip(nums2, range(n)), key=lambda x: x[0], reverse=True)
+        res = [0] * n
+        lo, hi = 0, n - 1
+        for e, i in buff:
+            if nums1[hi] > e:
+                res[i] = nums1[hi]
                 hi -= 1
+            else:
+                res[i] = nums1[lo]
+                lo += 1
         return res
