3a17f5e00a3d17ab769f1750f88b03d6975e80c0
yubowen.ssr <yubowen.ssr@bytedance.com>
2023-03-02 20:12:13 +0800

    提交 912 题

--- 0912. Sort an Array/Solution.py
+++ 0912. Sort an Array/Solution.py
@@ -1,3 +1,4 @@
+import random
 from typing import List
 
 
@@ -5,19 +6,23 @@ class Solution:
     def quickSort(self, nums: List[int], left: int, right: int) -> None:
         if left >= right:
             return
-        if all(nums[i] == nums[left] for i in range(left + 1, right + 1)):
-            return
-
-        mid = left + ((right - left) >> 1)
-        nums[left], nums[mid] = nums[mid], nums[left]
-        pivot, last = nums[left], left
-        for i in range(left + 1, right + 1):
-            if nums[i] < pivot:
-                last += 1
-                nums[i], nums[last] = nums[last], nums[i]
-        nums[left], nums[last] = nums[last], nums[left]
-        self.quickSort(nums, left, last - 1)
-        self.quickSort(nums, last + 1, right)
+        rand_idx = random.randint(left, right)
+        pivoit = nums[rand_idx]
+        nums[left], nums[rand_idx] = nums[rand_idx], nums[left]
+        i, j, k = left + 1, left, right + 1
+        while i < k:
+            if nums[i] < pivoit:
+                nums[i], nums[j + 1] = nums[j + 1], nums[i]
+                j += 1
+                i += 1
+            elif nums[i] > pivoit:
+                nums[i], nums[k - 1] = nums[k - 1], nums[i]
+                k -= 1
+            else:
+                i += 1
+        nums[left], nums[j] = nums[j], nums[left]
+        self.quickSort(nums, left, j - 1)
+        self.quickSort(nums, k, right)
 
     def sortArray_MK1(self, nums: List[int]) -> List[int]:
         """
@@ -57,4 +62,43 @@ class Solution:
         Merge Sort
         """
         self.mergeSort(nums, 0, len(nums) - 1)
+        return nums
+
+    def siftDown(self, nums: List[int], start: int, end: int) -> None:
+        parent = start
+        child = parent * 2 + 1
+        while child <= end:
+            if child + 1 <= end and nums[child] < nums[child + 1]:
+                child += 1
+            if nums[parent] >= nums[child]:
+                return
+            else:
+                nums[parent], nums[child] = nums[child], nums[parent]
+                parent = child
+                child = parent * 2 + 1
+
+    def sortArray_MK3(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        i = (n - 2) // 2
+        while i >= 0:
+            self.siftDown(nums, i, n - 1)
+            i -= 1
+
+        i = len(nums) - 1
+        while i > 0:
+            nums[0], nums[i] = nums[i], nums[0]
+            i -= 1
+            self.siftDown(nums, 0, i)
+        return nums
+
+    def sortArray_MK4(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        step = n // 2
+        while step > 0:
+            for i in range(step, n):
+                j = i - step
+                while j >= 0 and nums[j] > nums[j + step]:
+                    nums[j], nums[j + step] = nums[j + step], nums[j]
+                    j -= step
+            step //= 2
         return nums
