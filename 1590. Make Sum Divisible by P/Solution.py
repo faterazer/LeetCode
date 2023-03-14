8cb33cd8f89d87707720a499ddf0409591aabd33
yubowen.ssr <yubowen.ssr@bytedance.com>
2023-03-14 10:59:55 +0800

    1590: 增加 Python 代码

---
+++ 1590. Make Sum Divisible by P/Solution.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def minSubarray(self, nums: List[int], p: int) -> int:
+        total = sum(nums) % p
+        if total == 0:
+            return 0
+
+        memo = {0: -1}
+        res, pre_sum = len(nums), 0
+        for i, x in enumerate(nums):
+            pre_sum = (pre_sum + x) % p
+            memo[pre_sum] = i
+            need = (pre_sum - total) % p
+            if need in memo:
+                res = min(res, i - memo[need])
