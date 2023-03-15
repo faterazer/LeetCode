0a530a7267f45bee5ca2d0092dacf4cae8df789b
yubowen.ssr <yubowen.ssr@bytedance.com>
2023-03-15 13:33:11 +0800

    109: 增加 Java 代码

---
+++ 0109. Convert Sorted List to Binary Search Tree/Solution.java
@@ -0,0 +1,67 @@
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    private ListNode globalHead;
+
+    private int getLength(ListNode head) {
+        int res = 0;
+        while (head != null) {
+            ++res;
+            head = head.next;
+        }
+        return res;
+    }
+
+    private TreeNode buildTree(int left, int right) {
+        if (left > right) {
+            return null;
+        }
+        int mid = left + (right - left) / 2;
+        TreeNode root = new TreeNode();
+        root.left = buildTree(left, mid - 1);
+        root.val = globalHead.val;
+        globalHead = globalHead.next;
+        root.right = buildTree(mid + 1, right);
+        return root;
+    }
+
+    public TreeNode sortedListToBST(ListNode head) {
+        globalHead = head;
+        int length = getLength(head);
+        return buildTree(0, length - 1);
+    }
+}
