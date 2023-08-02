41d341a3cf3b7f7d99d9bd21ef66d812917473f8
yubowen.ssr <yubowen.ssr@bytedance.com>
2023-08-02 22:17:41 +0800

    提交 1757 题

---
+++ 1757. Recyclable and Low Fat Products/Solution.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+
+def find_products(products: pd.DataFrame) -> pd.DataFrame:
+    return products.loc[(products["low_fats"] == "Y") & (products["recyclable"] == "Y"), ["product_id"]]
