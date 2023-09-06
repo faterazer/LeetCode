86014b74e89b92635f33231af28e45902b4a8fe3
fater <faterazer@outlook.com>
2023-09-06 21:31:08 +0800

    提交 865 题

---
+++ 0865. Smallest Subtree with all the Deepest Nodes/Solution.cpp
@@ -0,0 +1,50 @@
+#include <utility>
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode()
+        : val(0)
+        , left(nullptr)
+        , right(nullptr)
+    {
+    }
+    TreeNode(int x)
+        : val(x)
+        , left(nullptr)
+        , right(nullptr)
+    {
+    }
+    TreeNode(int x, TreeNode* left, TreeNode* right)
+        : val(x)
+        , left(left)
+        , right(right)
+    {
+    }
+};
+
+class Solution {
+public:
+    TreeNode* subtreeWithAllDeepest(TreeNode* root)
+    {
+        return dfs(root).second;
+    }
+
+private:
+    pair<int, TreeNode*> dfs(TreeNode* root)
+    {
+        if (!root)
+            return { 0, nullptr };
+        pair<int, TreeNode*> left = dfs(root->left);
+        pair<int, TreeNode*> right = dfs(root->right);
+        if (left.first > right.first)
+            return { left.first + 1, left.second };
+        else if (left.first < right.first)
+            return { right.first + 1, right.second };
+        else
+            return { left.first + 1, root };
+    }
+};
