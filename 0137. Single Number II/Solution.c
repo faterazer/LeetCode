cbccd67e6b3def747d6674a8fedc87e8b07da5d8
yubowen.ssr <yubowen.ssr@bytedance.com>
2023-10-19 11:51:31 +0800

    137: 增加 C 代码

---
+++ 0137. Single Number II/Solution.c
@@ -0,0 +1,11 @@
+#include <limits.h>
+
+int singleNumber(int* nums, int numsSize)
+{
+    int a = 0, b = 0;
+    for (int i = 0; i < numsSize; i++) {
+        b = ~a & (b ^ nums[i]);
+        a = ~b & (a ^ nums[i]);
+    }
+    return b;
+}
