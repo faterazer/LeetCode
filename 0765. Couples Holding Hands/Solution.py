6b635d5e7339a1005e839a5d2845ab55a8ff305a
yubowen.ssr <yubowen.ssr@bytedance.com>
2023-11-15 11:43:27 +0800

    提交 765 题

--- 0765. Couples Holding Hands/Solution.py
+++ 0765. Couples Holding Hands/Solution.py
@@ -1,18 +1,20 @@
 from typing import List
 
+
 class Solution:
     def minSwapsCouples(self, row: List[int]) -> int:
-        buff = [(min(a, b), max(a, b)) for a, b in zip(row[::2], row[1::2])]
-        buff.sort(key=lambda x: x[0])
-        
-        row = []
-        for a, b in buff:
-            row.append(a)
-            row.append(b)
-        
+        pos = [0] * len(row)
+        for i, e in enumerate(row):
+            pos[e] = i
+
         res = 0
-        for i in range(len(row)):
-            while row[i] != i:
-                row[i], row[row[i]] = row[row[i]], row[i]
+        get_partner = lambda x: x ^ 1
+        for i in range(0, len(row) - 1, 2):
+            partner, j = get_partner(row[i]), i + 1
+            while row[j] != partner:
+                partner2 = get_partner(row[j])
+                pos2 = pos[partner2] ^ 1
+                pos[row[j]], pos[partner2] = pos[partner2], pos[row[j]]
+                row[j], row[pos2] = row[pos2], row[j]
                 res += 1
         return res
