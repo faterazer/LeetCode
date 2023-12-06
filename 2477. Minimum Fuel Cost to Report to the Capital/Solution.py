afe9d4d00741d97bd52648d34c5b2ab707f5051b
yubowen.ssr <yubowen.ssr@bytedance.com>
2023-12-06 18:46:01 +0800

    2477: 增加 Python 代码

--- 2477. Minimum Fuel Cost to Report to the Capital/Solution.py
+++ 2477. Minimum Fuel Cost to Report to the Capital/Solution.py
@@ -1,24 +1,25 @@
 from collections import defaultdict
 from typing import List
 
+
 class Solution:
     def minimumFuelCost(self, roads: List[List[int]], seats: int) -> int:
         graph = defaultdict(list)
         for a, b in roads:
             graph[a].append(b)
             graph[b].append(a)
-        
+
         res = 0
-        
+
         def dfs(node: int, parent: int) -> int:
             size = 1
             for child in graph[node]:
-                if child!= parent:
+                if child != parent:
                     size += dfs(child, node)
             if node != 0:
                 nonlocal res
-                res +=  (size - 1) // seats + 1
+                res += (size - 1) // seats + 1
             return size
-            
+
         dfs(0, -1)
         return res
