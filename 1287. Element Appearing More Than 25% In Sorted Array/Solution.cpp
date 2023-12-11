c771f25102677183ae37dfc1db641981453cdf74
yubowen.ssr <yubowen.ssr@bytedance.com>
2023-12-11 15:50:20 +0800

    1287: 更新代码

--- 1287. Element Appearing More Than 25% In Sorted Array/Solution.cpp
+++ 1287. Element Appearing More Than 25% In Sorted Array/Solution.cpp
@@ -2,15 +2,14 @@
 #include <vector>
 using namespace std;
 
-class Solution
-{
+class Solution {
 public:
-    int findSpecialInteger(vector<int> &arr)
+    int findSpecialInteger(vector<int>& arr)
     {
-        int gap = arr.size() / 4;
-        for (const int &i : {gap, gap * 2, gap * 3}) {
+        int n = arr.size();
+        for (const int& i : { n / 4, n * 2 / 4, n * 3 / 4 }) {
             auto p = equal_range(arr.begin(), arr.end(), arr[i]);
-            if (p.second - p.first > gap)
+            if (4 * distance(p.first, p.second) > n)
                 return arr[i];
         }
         return 0;
--- 1287. Element Appearing More Than 25% In Sorted Array/Solution.py
+++ 1287. Element Appearing More Than 25% In Sorted Array/Solution.py
@@ -4,8 +4,8 @@ from typing import List
 
 class Solution:
     def findSpecialInteger(self, arr: List[int]) -> int:
-        gap = len(arr) // 4
-        for i in [gap, gap * 2, gap * 3]:
-            if bisect.bisect_right(arr, arr[i]) - bisect.bisect_left(arr, arr[i]) > gap:
+        n = len(arr)
+        for i in [n // 4, n * 2 // 4, n * 3 // 4]:
+            if 4 * (bisect.bisect_right(arr, arr[i]) - bisect.bisect_left(arr, arr[i])) > n:
                 return arr[i]
         return 0
