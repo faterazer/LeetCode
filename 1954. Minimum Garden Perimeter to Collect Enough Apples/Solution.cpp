2d62b2b1223e17ad51e09c13917e7d3b45eb9226
yubowen.ssr <yubowen.ssr@bytedance.com>
2023-12-25 14:04:09 +0800

    1954: 增加 C++ 代码

---
+++ 1954. Minimum Garden Perimeter to Collect Enough Apples/Solution.cpp
@@ -0,0 +1,13 @@
+#include <cmath>
+using namespace std;
+
+class Solution {
+public:
+    long long minimumPerimeter(long long neededApples)
+    {
+        long long n = cbrt(neededApples / 4.0);
+        if (2 * n * (n + 1) * (2 * n + 1) < neededApples)
+            ++n;
+        return 8 * n;
+    }
+};
