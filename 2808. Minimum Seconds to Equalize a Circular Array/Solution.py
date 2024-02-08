19a819c0b0324280c96f3d35248341b0fed6e637
yubowen.ssr <yubowen.ssr@bytedance.com>
2024-02-08 14:48:17 +0800

    2808: 更新 Python 代码

--- 2808. Minimum Seconds to Equalize a Circular Array/Solution.py
+++ 2808. Minimum Seconds to Equalize a Circular Array/Solution.py
@@ -10,7 +10,7 @@ class Solution:
             ids_map[e].append(i)
 
         res = n = len(nums)
-        for k, ids in ids_map.items():
+        for ids in ids_map.values():
             ids.append(n + ids[0])
             max_gap = max(j - i for i, j in itertools.pairwise(ids))
             res = min(res, max_gap)
