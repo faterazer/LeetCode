c5e144b74bfd0709de6ec80d785dd6fa5eb84923
yubowen.ssr <yubowen.ssr@bytedance.com>
2024-03-12 11:42:01 +0800

    提交 1261 题

---
+++ 1261. Find Elements in a Contaminated Binary Tree/Solution.cpp
@@ -0,0 +1,72 @@
+#include <bit>
+#include <stack>
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ */
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode()
+        : val(0)
+        , left(nullptr)
+        , right(nullptr)
+    {
+    }
+    TreeNode(int x)
+        : val(x)
+        , left(nullptr)
+        , right(nullptr)
+    {
+    }
+    TreeNode(int x, TreeNode* left, TreeNode* right)
+        : val(x)
+        , left(left)
+        , right(right)
+    {
+    }
+};
+
+class FindElements {
+public:
+    FindElements(TreeNode* root)
+        : tree(root)
+    {
+        recover(root, 0);
+    }
+
+    bool find(int target)
+    {
+        TreeNode *cur = tree;
+        unsigned x = target + 1;
+        for (int i = 30 - countl_zero(x); i >= 0; --i) {
+            if ((x >> i) & 1)
+                cur = cur->right;
+            else
+                cur = cur->left;
+            if (!cur)
+                return false;
+        }
+        return true;
+    }
+
+private:
+    void recover(TreeNode* root, int val)
+    {
+        if (!root)
+            return;
+        root->val = val;
+        recover(root->left, 2 * val + 1);
+        recover(root->right, 2 * val + 2);
+    }
+
+    TreeNode* tree = nullptr;
+};
+
+/**
+ * Your FindElements object will be instantiated and called as such:
+ * FindElements* obj = new FindElements(root);
+ * bool param_1 = obj->find(target);
+ */
---
+++ 1261. Find Elements in a Contaminated Binary Tree/Solution.py
@@ -0,0 +1,38 @@
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class FindElements:
+    def __init__(self, root: Optional[TreeNode]):
+        self.root = root
+
+        def recover(node: TreeNode, x: int) -> None:
+            if not node:
+                return
+            node.val = x
+            recover(node.left, 2 * x + 1)
+            recover(node.right, 2 * x + 2)
+
+        recover(self.root, 0)
+
+    def find(self, target: int) -> bool:
+        target += 1
+        cur = self.root
+        for i in range(target.bit_length() - 2, -1, -1):
+            bit = (target >> i) & 1
+            cur = cur.right if bit else cur.left
+            if cur is None:
+                return False
+        return True
+
+
+# Your FindElements object will be instantiated and called as such:
+# obj = FindElements(root)
+# param_1 = obj.find(target)
