c376f3485eaa8d0b80e7eb854bf49f7eda04e0a0
yubowen.ssr <yubowen.ssr@bytedance.com>
2024-05-24 17:18:28 +0800

    提交 1673 题

---
+++ 1673. Find the Most Competitive Subsequence/Solution.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> mostCompetitive(vector<int>& nums, int k)
+    {
+        vector<int> mono_stk, res;
+        for (size_t i = 0; i < nums.size(); i++) {
+            while (!mono_stk.empty() && nums[i] < mono_stk.back() && nums.size() - i + mono_stk.size() > k)
+                mono_stk.pop_back();
+
+            if (mono_stk.size() < k)
+                mono_stk.emplace_back(nums[i]);
+        }
+        return mono_stk;
+    }
+};
