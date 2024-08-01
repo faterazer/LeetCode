f4bbeb11be73ee39826786c83935eeee929b65d5
yubowen.ssr <yubowen.ssr@bytedance.com>
2024-08-01 16:13:06 +0800

    提交 1395 题

--- 1395. Count Number of Teams/Solution.py
+++ 1395. Count Number of Teams/Solution.py
@@ -2,7 +2,8 @@ from typing import List
 
 
 class Solution:
-    def numTeams(self, rating: List[int]) -> int:
+    # 枚举中间点。时间复杂度：O(n^2)，空间复杂度：O(1)
+    def numTeams_MK1(self, rating: List[int]) -> int:
         n, res = len(rating), 0
         for j in range(n):
             iless = imore = kless = kmore = 0
@@ -17,4 +18,36 @@ class Solution:
                 elif rating[k] > rating[j]:
                     kmore += 1
             res += iless * kmore + imore * kless
+        return res
+
+    # 使用树状数组（Binary Indexed Tree）。时间复杂度：O(nlgn)，空间复杂度：O(n)
+    def numTeams_MK2(self, rating: List[int]) -> int:
+        def updateBIT(BIT: List[int], index: int, val: int) -> None:
+            while index < len(BIT):
+                BIT[index] += val
+                index += index & (-index)
+
+        def getPrefixSum(BIT: List[int], index: int) -> int:
+            res = 0
+            while index > 0:
+                res += BIT[index]
+                index -= index & (-index)
+            return res
+
+        max_rating = max(rating)
+        left_BIT, right_BIT = [0] * (max_rating + 1), [0] * (max_rating + 1)
+
+        for x in rating:
+            updateBIT(right_BIT, x, 1)
+
+        res = 0
+        for x in rating:
+            updateBIT(right_BIT, x, -1)
+            left_less = getPrefixSum(left_BIT, x - 1)
+            left_more = getPrefixSum(left_BIT, max_rating) - getPrefixSum(left_BIT, x)
+            right_less = getPrefixSum(right_BIT, x - 1)
+            right_more = getPrefixSum(right_BIT, max_rating) - getPrefixSum(right_BIT, x)
+            updateBIT(left_BIT, x, 1)
+
+            res += left_less * right_more + left_more * right_less
         return res
