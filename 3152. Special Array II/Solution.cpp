e24b1596f52e4dead1e53a0c5359fa59748a20a3
fater <faterazer@outlook.com>
2024-08-23 22:57:04 +0800

    3152: 修改 C++ 代码

--- 3152. Special Array II/Solution.cpp
+++ 3152. Special Array II/Solution.cpp
@@ -3,16 +3,13 @@ using namespace std;
 
 class Solution {
 public:
-    vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries)
+    vector<bool>
+    isArraySpecial(vector<int>& nums, vector<vector<int>>& queries)
     {
         vector<int> last_same(nums.size());
-        int last = 0;
         last_same[0] = 0;
-        for (size_t i = 1; i < nums.size(); i++) {
-            if ((nums[i] & 1) == (nums[i - 1] & 1))
-                last = i;
-            last_same[i] = last;
-        }
+        for (size_t i = 1; i < nums.size(); i++)
+            last_same[i] = (nums[i] & 1) == (nums[i - 1] & 1) ? i : last_same[i - 1];
 
         vector<bool> res(queries.size());
         for (size_t i = 0; i < queries.size(); i++)
