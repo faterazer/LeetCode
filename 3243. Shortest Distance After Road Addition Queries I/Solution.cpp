c105bd965360ea42630a256562458fc236df9a8b
yubowen.ssr <yubowen.ssr@bytedance.com>
2024-11-23 16:21:28 +0800

    3243: 更新 C++ 代码

--- 3243. Shortest Distance After Road Addition Queries I/Solution.cpp
+++ 3243. Shortest Distance After Road Addition Queries I/Solution.cpp
@@ -6,7 +6,7 @@ class Solution {
 public:
     vector<int> shortestDistanceAfterQueries(int n, vector<vector<int>>& queries)
     {
-        vector<vector<int>> from(n);
+        vector<vector<int>> shortcuts(n);
         vector<int> f(n);
         iota(f.begin(), f.end(), 0);
 
@@ -14,13 +14,13 @@ public:
         ans.reserve(queries.size());
         for (const vector<int>& query : queries) {
             int u = query[0], v = query[1];
-            from[v].emplace_back(u);
-            if (f[u] + 1 < f[v]) {
+            shortcuts[u].emplace_back(v);
+            if (f[v] > f[u] + 1) {
                 f[v] = f[u] + 1;
-                for (int i = v + 1; i < n; ++i) {
+                for (int i = v; i < n; ++i) {
                     f[i] = min(f[i], f[i - 1] + 1);
-                    for (int j : from[i])
-                        f[i] = min(f[i], f[j] + 1);
+                    for (int j : shortcuts[i])
+                        f[j] = min(f[j], f[i] + 1);
                 }
             }
             ans.emplace_back(f[n - 1]);
