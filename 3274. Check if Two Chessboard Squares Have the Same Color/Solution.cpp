2eb5ccb562ce4200cf66733bb3248b4e638857a6
yubowen.ssr <yubowen.ssr@bytedance.com>
2024-12-03 13:23:22 +0800

    提交 3274 题

---
+++ 3274. Check if Two Chessboard Squares Have the Same Color/Solution.cpp
@@ -0,0 +1,10 @@
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    bool checkTwoChessboards(string coordinate1, string coordinate2)
+    {
+        return ((coordinate1[0] ^ coordinate1[1]) & 1) == ((coordinate2[0] ^ coordinate2[1]) & 1);
+    }
+};
