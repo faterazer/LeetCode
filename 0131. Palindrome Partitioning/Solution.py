66454ea7afc0a32eaf18cd172488670e05714d62
fater <faterazer@outlook.com>
2025-03-10 17:48:42 +0800

    131: 更新 Python 代码

--- 0131. Palindrome Partitioning/Solution.py
+++ 0131. Palindrome Partitioning/Solution.py
@@ -1,15 +1,20 @@
 class Solution:
     def partition(self, s: str) -> list[list[str]]:
-        ans = []
-        path = []
+        n = len(s)
+        is_palindrome = [[True] * n for _ in range(n)]
+        for j in range(1, n):
+            for i in range(0, j):
+                is_palindrome[i][j] = s[i] == s[j] and (j - i < 2 or is_palindrome[i + 1][j - 1])
+
+        ans, path = [], []
 
         def dfs(i: int) -> None:
-            if i == len(s):
+            if i == n:
                 ans.append(path.copy())
                 return
 
-            for j in range(i, len(s)):
-                if s[i : j + 1] == s[i : j + 1][::-1]:
+            for j in range(i, n):
+                if is_palindrome[i][j]:
                     path.append(s[i : j + 1])
                     dfs(j + 1)
                     path.pop()
