485085549f012a2cd5d0028db54016914c9fe6e3
fater <faterazer@outlook.com>
2025-03-16 20:20:14 +0800

    [2560]: 更新 C++ 代码

--- 2560. House Robber IV/Solution.cpp
+++ 2560. House Robber IV/Solution.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 class Solution {
 public:
-    int minCapability(vector<int>& nums, int k)
+    int minCapability_MK1(vector<int>& nums, int k) // 二分 + DP
     {
         function<bool(int)> check = [&](int mx) -> bool {
             int f0 = 0, f1 = 0;
@@ -20,6 +20,31 @@ public:
                 }
             }
             return f1 >= k;
+        };
+
+        auto p = ranges::minmax(nums);
+        int left = p.min, right = p.max;
+        while (left < right) {
+            int mid = left + ((right - left) >> 1);
+            if (check(mid))
+                right = mid;
+            else
+                left = mid + 1;
+        }
+        return left;
+    }
+
+    int minCapability_MK2(vector<int>& nums, int k) // 二分 + 贪心
+    {
+        function<bool(int)> check = [&](int mx) -> bool {
+            int cnt = 0;
+            for (size_t i = 0; i < nums.size(); ++i) {
+                if (nums[i] <= mx) {
+                    ++cnt;
+                    ++i;
+                }
+            }
+            return cnt >= k;
         };
 
         auto p = ranges::minmax(nums);
