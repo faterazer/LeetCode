b2ba5e61086617a4f491b7388b08ba2e19b8619e
fater <faterazer@outlook.com>
2025-04-15 23:00:08 +0800

    [1534]: refine python 代码

--- 1534. Count Good Triplets/Solution.py
+++ 1534. Count Good Triplets/Solution.py
@@ -1,8 +1,5 @@
-from typing import List
-
-
 class Solution:
-    def countGoodTriplets(self, arr: List[int], a: int, b: int, c: int) -> int:
+    def countGoodTriplets_MK1(self, arr: list[int], a: int, b: int, c: int) -> int:
         res = 0
         preSum = [0] * 1001
         for j in range(len(arr)):
@@ -18,3 +15,26 @@ class Solution:
             for x in range(arr[j], 1001):
                 preSum[x] += 1
         return res
+
+    def countGoodTriplets_MK2(self, arr: list[int], a: int, b: int, c: int) -> int:
+        indices = sorted(range(len(arr)), key=lambda i: arr[i])
+
+        ans = 0
+        for j in indices:
+            y = arr[j]
+            left, right = [], []
+            for i in indices:
+                if i < j and abs(arr[i] - y) <= a:
+                    left.append(arr[i])
+            for k in indices:
+                if k > j and abs(y - arr[k]) <= b:
+                    right.append(arr[k])
+
+            k1 = k2 = 0
+            for x in left:
+                while k2 < len(right) and right[k2] - x <= c:
+                    k2 += 1
+                while k1 < len(right) and right[k1] - x < -c:
+                    k1 += 1
+                ans += k2 - k1
+        return ans
