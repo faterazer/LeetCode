254f105f06e6af680ea999eaa99c855828ab4df6
fater <faterazer@outlook.com>
2025-04-23 19:41:39 +0800

    [1399]: 增加数位 DP 解法

--- 1399. Count Largest Group/Solution.cpp
+++ 1399. Count Largest Group/Solution.cpp
@@ -1,10 +1,12 @@
+#include <functional>
 #include <unordered_map>
+#include <vector>
 using namespace std;
 
-class Solution
-{
+class Solution {
 public:
-    int countLargestGroup(int n)
+    // 暴力
+    int countLargestGroup_MK1(int n)
     {
         int res = 0, max = 0, tmp;
         unordered_map<int, int> counter;
@@ -19,6 +21,42 @@ public:
             }
         }
         return res;
+    }
+
+    // 数位 DP
+    int countLargestGroup_MK2(int n)
+    {
+        string s = to_string(n);
+        int m = s.size();
+
+        vector<vector<int>> memo(m, vector<int>(m * 9 + 1, -1));
+        function<int(int, int, bool)> dfs = [&](int i, int remain, bool limit_high) -> int {
+            if (i == m)
+                return remain == 0;
+
+            if (!limit_high && memo[i][remain] != -1)
+                return memo[i][remain];
+
+            int lo = 0, hi = limit_high ? s[i] - '0' : 9;
+            int ans = 0;
+            for (int d = lo; d <= min(hi, remain); ++d)
+                ans += dfs(i + 1, remain - d, limit_high && d == hi);
+            if (!limit_high)
+                memo[i][remain] = ans;
+            return ans;
+        };
+
+        int mx = 0, ans = 0;
+        for (int i = 1; i <= 9 * m; ++i) {
+            int cnt = dfs(0, i, true);
+            if (cnt > mx) {
+                mx = cnt;
+                ans = 1;
+            } else if (cnt == mx) {
+                ++ans;
+            }
+        }
+        return ans;
     }
 
 private:
--- 1399. Count Largest Group/Solution.py
+++ 1399. Count Largest Group/Solution.py
@@ -1,6 +1,35 @@
 import statistics
+from functools import cache
 
 
 class Solution:
-    def countLargestGroup(self, n: int) -> int:
+    # 暴力
+    def countLargestGroup_MK1(self, n: int) -> int:
         return len(statistics.multimode(sum(map(int, str(i))) for i in range(1, n + 1)))
+
+    # 数位DP
+    def countLargestGroup_MK2(self, n: int) -> int:
+        s = list(map(int, str(n)))
+        m = len(s)
+
+        @cache
+        def dfs(i: int, remain: int, limit_high: bool) -> int:
+            if i == m:
+                return remain == 0
+
+            lo = 0
+            hi = s[i] if limit_high else 9
+            ans = 0
+            for d in range(lo, hi + 1):
+                ans += dfs(i + 1, remain - d, limit_high and d == hi)
+            return ans
+
+        mx = ans = 0
+        for target in range(1, 9 * m + 1):
+            cnt = dfs(0, target, True)
+            if cnt > mx:
+                mx = cnt
+                ans = 1
+            elif cnt == mx:
+                ans += 1
+        return ans
