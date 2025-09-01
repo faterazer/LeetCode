98d12956316b08ff69d121c7723d6c8f20408bef
fater <faterazer@outlook.com>
2025-09-01 14:32:03 +0800

    提交 1791 题

---
+++ 1791. Find Center of Star Graph/Solution.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int findCenter(vector<vector<int>>& edges)
+    {
+        return edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1] ? edges[0][0] : edges[0][1];
+    }
+};
