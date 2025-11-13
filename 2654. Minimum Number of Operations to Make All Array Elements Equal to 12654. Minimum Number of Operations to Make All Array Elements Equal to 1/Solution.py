1189a57961f0b2bc1a6eba3d5739979de57df677
fater <faterazer@outlook.com>
2025-11-13 16:35:11 +0800

    2654: 增加 Python 代码

--- 2654. Minimum Number of Operations to Make All Array Elements Equal to 12654. Minimum Number of Operations to Make All Array Elements Equal to 1/Solution.py
+++ 2654. Minimum Number of Operations to Make All Array Elements Equal to 12654. Minimum Number of Operations to Make All Array Elements Equal to 1/Solution.py
@@ -1,19 +1,54 @@
-from itertools import pairwise
-from math import gcd, inf
+from math import gcd
 
 
 class Solution:
-    def minOperations(self, nums: list[int]) -> int | float:
+    def minOperations_MK1(self, nums: list[int]) -> int:
+        n = len(nums)
+        gcd_all = cnt1 = 0
+        for x in nums:
+            gcd_all = gcd(gcd_all, x)
+            cnt1 += x == 1
+        if gcd_all > 1:
+            return -1
+        if cnt1:
+            return n - cnt1
 
-        def helper(x: int, y: int) -> int | float:
-            g = gcd(x, y)
-            if g == 1:
-                return 1
-            if g == x or g == y:
-                return inf
-            return 1 + min(helper(x, gcd(x, y)), helper(y, gcd(x, y)))
+        min_size = n
+        for i in range(n):
+            g = 0
+            for j in range(i, n):
+                g = gcd(g, nums[j])
+                if g == 1:
+                    min_size = min(min_size, j - i)
+                    break
+        return min_size + n - 1
 
-        ans = inf
-        for a, b in pairwise(nums):
-            ans = min(ans, helper(a, b))
-        return ans + len(nums) - 1 if ans != inf else -1
+    def minOperations_MK2(self, nums: list[int]) -> int:
+        n = len(nums)
+        gcd_all = cnt1 = 0
+        for x in nums:
+            gcd_all = gcd(gcd_all, x)
+            cnt1 += x == 1
+        if gcd_all > 1:
+            return -1
+        if cnt1:
+            return n - cnt1
+
+        min_size = n
+        g = []
+        for i, x in enumerate(nums):
+            g.append([x, i])
+
+            j = 0
+            for p in g:
+                p[0] = gcd(p[0], x)
+                if g[j][0] == p[0]:
+                    g[j][1] = p[1]
+                else:
+                    j += 1
+                    g[j] = p
+            del g[j + 1 :]
+
+            if g[0][0] == 1:
+                min_size = min(min_size, i - g[0][1])
+        return min_size + n - 1
