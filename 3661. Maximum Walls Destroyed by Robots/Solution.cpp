631539130853b0a6aab7efabade56ca987c0f45a
fater <faterazer@outlook.com>
2025-12-04 14:46:03 +0800

    [3661]: 更新 C++ 代码

--- 3661. Maximum Walls Destroyed by Robots/Solution.cpp
+++ 3661. Maximum Walls Destroyed by Robots/Solution.cpp
@@ -10,15 +10,16 @@ public:
     int maxWalls(vector<int>& robots, vector<int>& distance, vector<int>& walls)
     {
         int n = robots.size();
-        vector<pair<int, int>> sorted_robots(n + 2);
-        sorted_robots[0] = { 0, 0 };
-        for (int i = 0; i < n; ++i)
-            sorted_robots[i + 1] = { robots[i], distance[i] };
-        sorted_robots[n + 1] = { numeric_limits<int>::max(), 0 };
+        vector<pair<int, int>> sorted_robots;
+        sorted_robots.reserve(n + 2);
+        sorted_robots.emplace_back(0, 0);
+        for (auto [r, d] : views::zip(robots, distance))
+            sorted_robots.emplace_back(r, d);
+        sorted_robots.emplace_back(numeric_limits<int>::max(), 0);
         ranges::sort(sorted_robots);
         ranges::sort(walls);
 
-        function<int(int, int)> count_walls = [&](int start, int end) -> int {
+        auto count_walls = [&](int start, int end) -> int {
             if (start > end)
                 return 0;
             return ranges::upper_bound(walls, end) - ranges::lower_bound(walls, start);
@@ -30,17 +31,17 @@ public:
             int curr_pos = sorted_robots[i].first, curr_dist = sorted_robots[i].second;
             int next_pos = sorted_robots[i + 1].first;
 
-            int left_limit = max(prev_pos + 1, curr_pos - curr_dist);
+            int left_limit = max(curr_pos - curr_dist, prev_pos + 1);
             int walls_on_left = count_walls(left_limit, curr_pos);
-            int candidate_from_left = dp[0] + walls_on_left;
+            int candidate_left = dp[0] + walls_on_left;
             int last_right_limit = min(prev_pos + prev_dist, curr_pos - 1);
-            int candidate_from_right = dp[1] + walls_on_left - count_walls(left_limit, last_right_limit);
+            int candidate_right = dp[1] + walls_on_left - count_walls(left_limit, last_right_limit);
 
-            int right_limit = min(next_pos - 1, curr_pos + curr_dist);
+            int right_limit = min(curr_pos + curr_dist, next_pos - 1);
             int walls_on_right = count_walls(curr_pos, right_limit);
             dp[1] = max(dp[0], dp[1]) + walls_on_right;
-            dp[0] = max(candidate_from_left, candidate_from_right);
+            dp[0] = max(candidate_left, candidate_right);
         }
-        return ranges::max(dp);
+        return max(dp[0], dp[1]);
     }
 };
