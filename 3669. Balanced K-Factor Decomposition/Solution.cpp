7f976c3f29f2b26aad118c70a733ea995815a915
fater <faterazer@outlook.com>
2025-12-07 15:21:35 +0800

    [3669]: 更新 C++ 代码

--- 3669. Balanced K-Factor Decomposition/Solution.cpp
+++ 3669. Balanced K-Factor Decomposition/Solution.cpp
@@ -12,43 +12,40 @@ public:
         vector<int> divisors;
         for (int i = 1; i * i <= n; ++i) {
             if (n % i == 0) {
-                divisors.emplace_back(i);
+                divisors.push_back(i);
                 if (i * i != n)
-                    divisors.emplace_back(n / i);
+                    divisors.push_back(n / i);
             }
         }
         ranges::sort(divisors);
 
-        int min_diff = numeric_limits<int>::max();
         vector<int> path(k), result;
+        int min_diff = numeric_limits<int>::max();
 
-        auto dfs = [&](this auto&& self, int i, int target) -> void {
-            // 只剩最后一个位置，没得选，只能是 target
+        auto dfs = [&](this auto&& self, int i, int target, int start_idx) -> void {
             if (i == k - 1) {
-                if (target - path[0] < min_diff) {
-                    min_diff = target - path[0];
+                int diff = target - path[0];
+                if (diff < min_diff) {
                     path[i] = target;
+                    min_diff = diff;
                     result = path;
                 }
                 return;
             }
 
             int max_d = sqrt(target);
-            for (int d : divisors) {
-                if (d > max_d || (i > 0 && d - path[0] >= min_diff))
+            for (size_t idx = start_idx; idx < divisors.size(); ++idx) {
+                int d = divisors[idx];
+                if (d > max_d || (i > 0 && d - path[0]) >= min_diff)
                     break;
-
-                if (target % d != 0)
-                    continue;
-
-                if (i == 0 || d >= path[i - 1]) {
+                if (target % d == 0) {
                     path[i] = d;
-                    self(i + 1, target / d);
+                    self(i + 1, target / d, idx);
                 }
             }
         };
 
-        dfs(0, n);
+        dfs(0, n, 0);
         return result;
     }
 };
