b525bda4ef1c4120aed99020a384ef1e28e50ea2
fater <faterazer@outlook.com>
2025-12-18 16:40:09 +0800

    提交 3573 题

---
+++ 3573. Best Time to Buy and Sell Stock V/Solution.cpp
@@ -0,0 +1,22 @@
+#include <algorithm>
+#include <array>
+#include <ranges>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    long long maximumProfit(vector<int>& prices, int k)
+    {
+        constexpr long long INF = numeric_limits<long long>::min() / 2;
+        vector<array<long long, 3>> dp(k + 2, { 0, INF, INF });
+        for (int p : prices) {
+            for (int i = k + 1; i > 0; --i) {
+                dp[i][1] = max(dp[i][1], dp[i][0] - p);
+                dp[i][2] = max(dp[i][2], dp[i][0] + p);
+                dp[i][0] = ranges::max({ dp[i][0], dp[i - 1][1] + p, dp[i - 1][2] - p });
+            }
+        }
+        return dp[k + 1][0];
+    }
+};
