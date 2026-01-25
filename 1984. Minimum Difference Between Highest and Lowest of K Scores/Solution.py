e0e241715fbb2445e35b20188cbbffb6314f5c12
fater <faterazer@outlook.com>
2026-01-25 16:03:30 +0800

    [1984]: 更新 Python 代码

--- 1984. Minimum Difference Between Highest and Lowest of K Scores/Solution.py
+++ 1984. Minimum Difference Between Highest and Lowest of K Scores/Solution.py
@@ -1,7 +1,4 @@
-from typing import List
-
-
 class Solution:
-    def minimumDifference(self, nums: List[int], k: int) -> int:
+    def minimumDifference(self, nums: list[int], k: int) -> int:
         nums.sort()
         return min(a - b for a, b in zip(nums[k-1:], nums))
