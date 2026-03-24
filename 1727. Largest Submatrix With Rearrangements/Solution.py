e38a96dd831dc88d4ec059c25f8293841d5cd31b
fater <faterazer@outlook.com>
2026-03-24 15:44:38 +0800

    [1727]: 增加 Python 代码

---
+++ 1727. Largest Submatrix With Rearrangements/Solution.py
@@ -0,0 +1,19 @@
+class Solution:
+    def largestSubmatrix(self, matrix: list[list[int]]) -> int:
+        n = len(matrix[0])
+        heights = [0] * n
+        ids = list(range(n))
+        result = 0
+        for i, row in enumerate(matrix):
+            zeros, non_zeros = [], []
+            for j in ids:
+                if row[j] == 0:
+                    heights[j] = 0
+                    zeros.append(j)
+                else:
+                    heights[j] += 1
+                    non_zeros.append(j)
+            ids = zeros + non_zeros
+            for i in range(len(zeros), n):
+                result = max(result, (n - i) * (heights[ids[i]]))
+        return result
